321974a51f3dae8783829fe15a32838e0c0580f4
Raul Ochoa <rochoaf@gmail.com>
2015-11-20 17:33:41 +0100

    Don't cache solid grids in MapnikSource
    See https://github.com/CartoDB/Windshaft/issues/310

--- test/grid.test.js
+++ test/grid.test.js
@@ -49,6 +49,7 @@ describe('Render ', function() {
             var count = 0;
             tileCoords.forEach(function(coords,idx,array) {
                 source.getGrid(coords[0], coords[1], coords[2], function(err, info, headers, stats) {
+                    assert.equal(Object.keys(source.solidCache).length, 0);
                     assert.ok(stats);
                     assert.ok(stats.hasOwnProperty('render'));
                     assert.ok(stats.hasOwnProperty('encode'));
